0bd46c1b0f1ac03378d7a33741473e30edb899d6
trankhacbinhduong <trankhacbinhduong@gmail.com>
2024-03-18 23:08:11 +0700

    :hammer: Setup dev environment

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
+require("dotenv").config();
+const morgan = require("morgan");
 const express = require("express");
 
 const app = express();
-const port = 8000;
+const port = process.env.PORT || 8000;
+
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
