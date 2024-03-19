9bd02988fb6f6a1bbeb05bcde22de4479da45b28
trankhacbinhduong <trankhacbinhduong@gmail.com>
2024-03-19 21:43:15 +0700

    :+1: Custom prisma unique constraint validation

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -1,8 +1,15 @@
 const errorHandler = (err, req, res, next) => {
   console.error(err);
 
+  // Custom error for Prisma unique constraint validation
+  if (err.code === "P2002") {
+    err.statusCode = 400;
+    err.message = `Duplicate ${err.meta.target.split("_").join(" ")}`;
+  }
+
   const statusCode = err.statusCode || 500;
   const message = err.message || "Internal server error";
+
   res.status(statusCode).json({ message });
 };
 
