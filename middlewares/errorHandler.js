50bdb0ec4e640e4371878b9d39d488434ff3a73d
trankhacbinhduong <trankhacbinhduong@gmail.com>
2024-03-19 21:18:49 +0700

    :bug: Fix typo statusCode in error handler

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -1,7 +1,7 @@
 const errorHandler = (err, req, res, next) => {
   console.error(err);
 
-  const statusCode = err.status || 500;
+  const statusCode = err.statusCode || 500;
   const message = err.message || "Internal server error";
   res.status(statusCode).json({ message });
 };
